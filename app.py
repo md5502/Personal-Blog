43aef0a481261b0ac55934b997eea882f7127196
md5502 <m.baniasadi.d@gmail.com>
2024-09-02 12:04:46 +0330

    create the app file

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+from flask_sqlalchemy import SQLAlchemy
+
+
+app =Flask(__name__)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite///database.sqlite'
+
+db = SQLAlchemy(app)
+
+if __name__ == "__main__":
+    app.run(debug=True)

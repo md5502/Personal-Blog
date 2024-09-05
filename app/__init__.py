c50968eda78c49f98417e2c05905801aaee4cd1d
md5502 <m.baniasadi.d@gmail.com>
2024-09-05 20:29:27 +0330

    add the auth blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from .config import Config
+from flask_httpauth import HTTPBasicAuth
+
+
 db = SQLAlchemy()
 migrate = Migrate()
-
+auth = HTTPBasicAuth()
 
 def create_app():
     app = Flask(__name__)
@@ -16,7 +19,7 @@ def create_app():
 
     from .routes import admin, auth, blog, user
 
-    # app.register_blueprint(auth.bp)
+    app.register_blueprint(auth.bp)
     app.register_blueprint(admin.bp)
     app.register_blueprint(blog.bp)
     # app.register_blueprint(user.bp)

78b74046b0ac8819e845b54bc39d2a452cd63a64
md5502 <m.baniasadi.d@gmail.com>
2024-09-05 21:05:25 +0330

    delete the duplicate files

--- utils.py
+++
@@ -1,9 +0,0 @@
-from app import db , User, generate_password_hash
-
-def create_super_user(username, password):
-    admin_user = User(username, generate_password_hash(password), "admin")
-    db.session.add(admin_user)
-    db.session.commit()
-    print("super user created successfully")
-
-create_super_user('admin', 'admin')

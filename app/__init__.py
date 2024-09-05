a4359f249508475254f34ec36b91baebb2e1c1ec
md5502 <m.baniasadi.d@gmail.com>
2024-09-05 19:40:32 +0330

    modify init file

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
+from .config import Config
 db = SQLAlchemy()
 migrate = Migrate()
 
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('config.Config')
+    app.config.from_object(Config)
 
     db.init_app(app)
     migrate.init_app(app)
@@ -16,9 +16,9 @@ def create_app():
 
     from .routes import admin, auth, blog, user
 
-    app.register_blueprint(auth.bp)
+    # app.register_blueprint(auth.bp)
     app.register_blueprint(admin.bp)
-    app.register_blueprint(blog.bp)
-    app.register_blueprint(user.bp)
+    # app.register_blueprint(blog.bp)
+    # app.register_blueprint(user.bp)
 
     return app

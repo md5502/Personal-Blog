d158edf99aa6bf93c165a327c2019d6186299763
md5502 <m.baniasadi.d@gmail.com>
2024-09-05 18:19:21 +0330

    create the init file for the app

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config.Config')
+
+    db.init_app(app)
+    migrate.init_app(app)
+
+
+    from .routes import admin, auth, blog, user
+
+    app.register_blueprint(auth.bp)
+    app.register_blueprint(admin.bp)
+    app.register_blueprint(blog.bp)
+    app.register_blueprint(user.bp)
+
+    return app
